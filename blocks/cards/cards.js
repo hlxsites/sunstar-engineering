97e57f920b7b5f3b065021860676ec29681ff515
Ghita Lucian <ghita@adobe.com>
2023-05-23 17:13:55 +0200

    Cards rendering
    * most of it in cards.css
    * card.js changed to render the card inside a link
    * importer not changed
    * tweaks to global style.css to remove round button style of links

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -6,13 +6,39 @@ export default function decorate(block) {
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
     li.innerHTML = row.innerHTML;
-    [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
-    });
+
+    // find the first a deep in the li
+    const a = li.querySelector('a');
+    if (a) {
+      const aContent = a.innerHTML;
+      const cardTitleDiv = document.createElement('div');
+      cardTitleDiv.innerHTML = aContent;
+      a.replaceWith(cardTitleDiv);
+      a.innerHTML = '';
+      a.append(...li.children);
+      li.append(a);
+      [...a.children].forEach((div) => {
+        if (div.children.length === 1 && div.querySelector('picture')) {
+          div.className = 'cards-card-image';
+        } else {
+          div.className = 'cards-card-body';
+        }
+      });
+    } else {
+      [...li.children].forEach((div) => {
+        if (div.children.length === 1 && div.querySelector('picture')) {
+          div.className = 'cards-card-image';
+        } else {
+          div.className = 'cards-card-body';
+        }
+      });
+    }
+
     ul.append(li);
   });
-  ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
+  ul.querySelectorAll('img')
+    .forEach((img) => img.closest('picture')
+      .replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
   block.textContent = '';
   block.append(ul);
 }
