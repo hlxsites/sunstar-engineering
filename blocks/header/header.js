4405cc9911e8b16d67d2e6f3b37e11b9094aa08a
Satya Deep Maheshwari <satyam@adobe.com>
2023-05-28 23:56:21 +0530

    Added social icons

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -1,5 +1,26 @@
 import { getMetadata, decorateIcons } from '../../scripts/lib-franklin.js';
 
+function decorateSocial(social) {
+  social.classList.add('social');
+  social.innerHTML = social.innerHTML.replace(/\[social\]/, '');
+}
+
+function decorateTopNav(nav) {
+  nav.querySelectorAll(':scope>ul>li').forEach((li) => {
+    if (li.textContent.trim() === '[social]') {
+      decorateSocial(li);
+    }
+  });
+}
+
+function decorateMiddleNav() {
+}
+
+function decorateBottomNav() {
+
+}
+
+const navDecorators = { 'nav-top': decorateTopNav, 'nav-middle': decorateMiddleNav, 'nav-bottom': decorateBottomNav };
 /**
  * decorates the header, mainly the nav
  * @param {Element} block The header block element
@@ -21,6 +42,7 @@ export default async function decorate(block) {
       const nav = document.createElement('nav');
       nav.classList.add(navClass);
       nav.innerHTML = fetchedNav.querySelectorAll(':scope>div')[idx].innerHTML;
+      navDecorators[navClass](nav);
       block.appendChild(nav);
     });
     decorateIcons(block);
