b7bfd23ce545e268eac66f3ec6360af9431d4da3
Ashish Chopra <ashishc@adobe.com>
2023-06-15 17:54:05 +0530

    adding background support

--- blocks/styled-para/styled-para.js
+++ blocks/styled-para/styled-para.js
@@ -5,21 +5,29 @@ export default async function decorate(block) {
   });
 
   block.querySelectorAll('.styled-para-1-cols').forEach((row) => {
-    row.querySelector('p')?.
-        parentElement?.classList.add('content');
-    row.querySelector('h1, h2, h3, h4, h5, h6')?.
-        parentElement?.classList.add('heading');
+    row.querySelector('p')
+      ?.parentElement?.classList.add('content');
+    row.querySelector('h1, h2, h3, h4, h5, h6')
+      ?.parentElement?.classList.add('heading');
   });
 
   block.querySelectorAll('.styled-para-2-cols').forEach((row) => {
     const typeHintEl = row.querySelector('div:first-child');
-    const typeHints = typeHintEl?.textContent?.
-        trim()?.toLowerCase()?.
-        split(',')?.map((type) => type.trim());
+    const typeHints = typeHintEl?.textContent
+      ?.trim()?.toLowerCase()
+      ?.split(',')?.map((type) => type.trim());
     if (typeHints?.length) {
       row.classList.add(...typeHints);
       typeHintEl.remove();
     }
+  });
+
+  block.querySelectorAll('.background').forEach((row) => {
+    const backgroundContainer = document.createElement('div');
+    backgroundContainer.classList.add('styled-para-background-container');
+    const pictureContainer = row.querySelector('div:first-child:has(picture)');
+    backgroundContainer.append(pictureContainer);
+    row.append(backgroundContainer);
   });
 
   block.querySelectorAll('.button-container > .button').forEach((a) => {
