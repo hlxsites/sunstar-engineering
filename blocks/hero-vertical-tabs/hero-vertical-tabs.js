125241cfd922a7e1046b3d3598aac8701db2eb56
JiangLong2019 <57093988+JiangLong2019@users.noreply.github.com>
2023-06-20 12:47:05 +0900

    Fix PR review issues
    1. Dropdown keeps shown when resize to desktop view.
    2. Refactoring js methods.
    3. Change name of common css styles for current block.
    4. Change block name

--- blocks/hero-vertical-tabs/hero-vertical-tabs.js
+++ blocks/hero-vertical-tabs/hero-vertical-tabs.js
@@ -1,20 +1,16 @@
-export default async function decorate(block) {
-  const tagHero = document.createElement('div');
-  tagHero.classList.add('tag-hero-bg');
-  const taglist = document.createElement('div');
-  taglist.classList.add('tag-hero-taglist');
+function buildTaglist(taglist, block) {
   const taglistLeft = document.createElement('div');
-  taglistLeft.classList.add('tag-hero-dropdown-left');
+  taglistLeft.classList.add('hero-vertical-tabs-dropdown-left');
   taglist.append(taglistLeft);
 
   const strongElements = block.querySelectorAll('ul strong a');
   const tagListTitle = document.createElement('div');
-  tagListTitle.classList.add('tag-hero-dropdown-title');
+  tagListTitle.classList.add('hero-vertical-tabs-dropdown-title');
   tagListTitle.textContent = strongElements[0].textContent;
   taglist.append(tagListTitle);
 
   const ul = document.createElement('ul');
-  ul.classList.add('tag-hero-dropdown');
+  ul.classList.add('hero-vertical-tabs-dropdown');
   block.querySelectorAll('ul li').forEach((li) => {
     if (li.querySelectorAll('strong').length !== 0) {
       const aLink = li.querySelectorAll('a')[0];
@@ -26,35 +22,48 @@ export default async function decorate(block) {
     ul.append(li);
   });
   taglist.addEventListener('click', () => {
-    if (ul.classList.contains('visible')) {
-      ul.classList.remove('visible');
-      taglist.classList.remove('visible');
-    } else {
-      ul.classList.add('visible');
-      taglist.classList.add('visible');
-    }
+    ul.classList.toggle('visible');
+    taglist.classList.toggle('visible');
   });
 
   window.addEventListener('click', (evt) => {
-    if (!evt.target.classList.contains('tag-hero-taglist')
+    if (!evt.target.classList.contains('hero-vertical-tabs-taglist')
       && ul.classList.contains('visible')) {
       ul.classList.remove('visible');
       taglist.classList.remove('visible');
     }
   });
-  taglist.append(ul);
 
-  const heroImage = document.createElement('div');
-  heroImage.classList.add('tag-hero-background-image');
+  const mediaQueryList = window.matchMedia('(min-width: 62rem)');
+  const listener = (evt) => {
+    if (evt.matches) {
+      ul.classList.remove('visible');
+      taglist.classList.remove('visible');
+    }
+  };
+  mediaQueryList.addEventListener('change', listener);
+  listener(mediaQueryList);
+  taglist.append(ul);
+}
+
+function buildImageAndContent(heroImage, block) {
   const imageContent = document.createElement('div');
-  imageContent.classList.add('tag-hero-content');
+  imageContent.classList.add('hero-vertical-tabs-content');
   if ([...block.children][2] != null && [...[...block.children][2].children][1] != null) {
     imageContent.append([...[...block.children][2].children][1]);
   }
   const picture = block.querySelectorAll('picture')[0];
   heroImage.append(picture);
   heroImage.append(imageContent);
-  tagHero.append(taglist);
-  tagHero.append(heroImage);
-  block.replaceChildren(tagHero);
+}
+
+export default async function decorate(block) {
+  const taglist = document.createElement('div');
+  const heroImage = document.createElement('div');
+  taglist.classList.add('hero-vertical-tabs-taglist');
+  heroImage.classList.add('hero-vertical-tabs-background-image');
+  buildTaglist(taglist, block);
+  buildImageAndContent(heroImage, block);
+  block.replaceChildren(taglist);
+  block.append(heroImage);
 }
