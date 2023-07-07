fd92d6eee452918d7b29d2f1d151acf597162bf4
Satya Deep Maheshwari <satyam@adobe.com>
2023-07-07 13:15:17 +0530

    Added optional tablet config

--- blocks/spacer/spacer.js
+++ blocks/spacer/spacer.js
@@ -3,5 +3,11 @@ import { readBlockConfig } from '../../scripts/lib-franklin.js';
 export default async function decorate(block) {
   const blockCfg = readBlockConfig(block);
   block.innerHTML = '';
-  block.style.height = window.innerWidth < 992 ? blockCfg.mobile : blockCfg.desktop;
+  if (window.innerWidth >= 1232) {
+    block.style.height = blockCfg.desktop;
+  } else if (window.innerWidth >= 992) {
+    block.style.height = blockCfg.tablet ? blockCfg.tablet : blockCfg.mobile;
+  } else {
+    block.style.height = blockCfg.mobile;
+  }
 }
